8fffba9c48868e54c48213f142b8693d0ce7198c
Andrew Wibel <andrew.wibel@gmail.com>
2016-02-16 21:33:01 -0800

    changed index route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get '/' => 'products#index'
+  get '/products' => 'products#index'
   get '/products/new' => 'products#new'
   get '/products/:id' => 'products#show'
   get '/products/:id/edit' => 'products#edit'
